d81e6f3aa58d21ab2c0e2bd3c5353d22b35da2fe
jae0_12 <113167437+jaeyoungmun@users.noreply.github.com>
2023-07-06 01:16:48 +0900

    로그인/마이페이지_문재영

--- static/js/find_id_pw.js
+++ static/js/find_id_pw.js
@@ -1,13 +1,13 @@
 function show_form_id() {
     $("#form_find_id").show();
     $("#form_find_pw").hide();
-    $("#form_find_id").css("background-color", "#FFBDBD");
-    $("#find_pwform_find_pw_button").css("background-color", "#FFE5E5");
+    $(".find_id_button").css("background-color", "#FFBDBD");
+    $(".find_pw_button").css("background-color", "#FFE5E5");
 }
 
 function show_form_pw() {
     $("#form_find_id").hide();
     $("#form_find_pw").show();
-    $("#form_find_id").css("background-color", "#FFE5E5");
-    $("#form_find_pw").css("background-color", "#FFBDBD");
+    $(".find_id_button").css("background-color", "#FFE5E5");
+    $(".find_pw_button").css("background-color", "#FFBDBD");
 }
