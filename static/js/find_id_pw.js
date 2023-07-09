0725886f5bd6d47bc6918319fdab1ce51a4cfe3c
jae0_12 <113167437+jaeyoungmun@users.noreply.github.com>
2023-07-10 03:25:32 +0900

    2차 수정(문재영)

--- static/js/find_id_pw.js
+++ static/js/find_id_pw.js
@@ -15,23 +15,23 @@
 function show_form_id(){
     var form_find_id = document.getElementById("form_find_id");
     var form_find_pw = document.getElementById("form_find_pw");
-    var find_id_button = document.getElementsByClassName("find_id_button");
-    var find_pw_button = document.getElementsByClassName("find_pw_button");
+    var find_id_button = document.getElementById("find_id_button");
+    var find_pw_button = document.getElementById("find_pw_button");
     
     form_find_id.style.display = "block";
     form_find_pw.style.display = "none";
-    find_id_button.style.backgroundColor = "FFBDBD";
-    find_pw_button.style.backgroundColor = "FFE5E5";
+    find_id_button.style.backgroundColor = "#FFBDBD";
+    find_pw_button.style.backgroundColor = "#FFE5E5";
 }
 
 function show_form_pw(){
     var form_find_id = document.getElementById("form_find_id");
     var form_find_pw = document.getElementById("form_find_pw");
-    var find_id_button = document.getElementsByClassName("find_id_button");
-    var find_pw_button = document.getElementsByClassName("find_pw_button");
+    var find_id_button = document.getElementById("find_id_button");
+    var find_pw_button = document.getElementById("find_pw_button");
     
     form_find_id.style.display = "none";
     form_find_pw.style.display = "block";
-    find_id_button.style.backgroundColor = "FFE5E5";
-    find_pw_button.style.backgroundColor = "FFBDBD";
+    find_id_button.style.backgroundColor = "#FFE5E5";
+    find_pw_button.style.backgroundColor = "#FFBDBD";
 }
